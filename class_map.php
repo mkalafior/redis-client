d962e45c3d8d5e859bd715707533ad73fc8e052a
sebakpl <sebakpl@gmail.com>
2015-06-18 15:56:20 +0200

    Mistake in global config fixed.

--- class_map.php
+++ class_map.php
@@ -1,4 +1,6 @@
 <?php
 return array(
     'Redis\Algorithms\CRC16' => __DIR__ . '/src/algorithms/CRC16.php',
+    'Redis\Connection\Phpiredis' => __DIR__ . '/src/connection/Phpiredis.php',
+    'Redis\Redis' => __DIR__ . '/src/Redis.php',
 );
