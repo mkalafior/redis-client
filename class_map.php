f8ce8d1d6a6737686ea1666bc14bacf707523a9d
Piotr Kugla <piku235@gmail.com>
2017-09-21 15:01:27 +0200

    Remove unused class_map.php

--- class_map.php
+++
@@ -1,6 +0,0 @@
-<?php
-return array(
-    'Redis\Algorithms\CRC16' => __DIR__ . '/src/algorithms/CRC16.php',
-    'Redis\Connection\Phpiredis' => __DIR__ . '/src/connection/Phpiredis.php',
-    'Redis\Redis' => __DIR__ . '/src/Redis.php',
-);
